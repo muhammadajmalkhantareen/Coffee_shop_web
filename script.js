4003d9179229d7daf87bb7eaea54b947cac55c92
Muhammad Ajmal Khan <muhammadajmalkhantareen@gnail.com>
2025-07-31 00:11:18 +0500

    added some codes js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // smooth scrolling for navigation links
-document.querySelectorAll('a[href="#"]').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).
